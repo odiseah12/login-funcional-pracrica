d73882b0a7c5f60523076c79caa6f61844b08d97
odiseah12 <113215622+odiseah12@users.noreply.github.com>
2023-02-26 12:42:34 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 function login() {
   let user = document.getElementById("user").value;
   let pass = document.getElementById("pass").value;
-  if(user == "damian" && pass == "1234"){
-    alert("bienvenido Damian")
+  if(user == "prueba" && pass == "1234"){
+    alert("Correcto")
   }else{
-    alert("no registrado")
+    alert("los datos ingresado son invalido")
   }
   
 }
